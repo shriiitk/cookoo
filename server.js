9234da5c363f8199d2de8b658c15ccc6b0c837bc
shriiitk <shri.iitk@gmail.com>
2014-07-03 21:15:05 +0530

    Favicon added

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
 app.configure(function() {
 	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+	app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 	app.use(express.logger('dev')); 						// log every request to the console
 	app.use(express.bodyParser()); 							// pull information from html in POST
 });
