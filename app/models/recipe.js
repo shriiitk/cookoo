a673166537fb6345c3e5b532c56c13101e38890c
shriiitk <shri.iitk@gmail.com>
2014-07-11 17:52:45 +0530

    chef name added in index

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -1,9 +1,6 @@
 // load mongoose since we need it to define a model
 var mongoose = require('mongoose');
 var textSearch = require('mongoose-text-search');
-// module.exports = mongoose.model('Tag', {
-// 	title	: { type: String, required: true, unique: true, trim: true, uppercase: true, }
-// });
 
 // create our schema
 var recipeSchema = mongoose.Schema({
@@ -28,14 +25,36 @@ recipeSchema.plugin(textSearch);
 recipeSchema.index({
     title		: "text",
     tags		: "text",
-    instructions: "text"
+    instructions: "text",
+    chef        : "text"
 }, {
     weights: {
         title		: 10,
         tags		: 5,
-        instructions: 1
+        instructions: 1,
+        chef        : 1
     },
-    name: "RecipeTextIndex"
+    name: "RecipeTextIndexWithChef"
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
+
+/*
+db.recipes.ensureIndex(
+ {
+   title    : "text",
+    tags    : "text",
+    instructions: "text",
+    chef    : "text"
+ },
+ {
+   weights: {
+        title   : 10,
+        tags    : 5,
+        instructions: 1,
+        chef        : 1
+            },
+   name: "RecipeTextIndexWithChef"
+ }
+);
+ */
